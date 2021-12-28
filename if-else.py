892cf28285f3f17eb16508da72fe1e3b581bd2b8
nurkhan8 <95960533+nurkhan8@users.noreply.github.com>
2021-12-28 22:36:06 +0600

    Add files via upload

---
+++ if-else.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+if (1 > a or a > 10 ^ 10) or (1 > b or b > 10 ^ 10):
+    print(a+b)
+    print(a-b)
+    print(a*b)
+else:
+    print(0)
